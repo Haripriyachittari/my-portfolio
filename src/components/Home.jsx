f251cdd79c8f410feca5fa7bc6143bf57a50ab9b
Haripriya Chittari <hpvarma09@gmail.com>
2023-01-09 17:12:43 +0530

    ...

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,59 +1,80 @@
-import React, { useEffect } from 'react'
-import {HiArrowNarrowRight} from 'react-icons/hi'
+import React, { useEffect } from "react";
+import { HiArrowNarrowRight } from "react-icons/hi";
 import { Link } from "react-scroll";
-import { FaLinkedin,FaGithub } from "react-icons/fa";
-import {BsFillPersonLinesFill} from 'react-icons/bs';
-import resume from '../assets/HARIPRIYA_CV_Resume.pdf';
-import Typewriter from 'typewriter-effect';
-import Aos from 'aos';
-import 'aos/dist/aos.css'
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+import resume from "../assets/HARIPRIYA_CV_Resume.pdf";
+import Typewriter from "typewriter-effect";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 const Home = () => {
-  useEffect(()=>{
-   Aos.init({duration:2000})
-  },[])
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
   return (
-    <div  name ='home' className='w-full h-screen bg-[#0a192f]'>
-        {/* Container */}
-        <div data-aos="fade-up" className='flex flex-col max-w-[1000px] h-full mx-auto px-8 justify-center '>
-            <p  className='text-pink-600 sm:mb-2'>Hi, my name is</p>
-            <h1 className='text-5xl sm:text-7xl font-bold text-[#dde5fa] mb-4'>Haripriya Chittari <span className='text-4xl md:text-5xl animate-wave absolute'>👋</span> </h1>
-            
-           
-            <h2 className='text-3xl sm:text-5xl font-bold text-pink-600 mb-2'>
-             <Typewriter
-                 options={{
-                 strings: ["Full Stack Developer"],
-                 autoStart: true,
-                 delay:180,
-                 loop: true,
-                  }}
-             />
-             
-            </h2>
+    <div name="home" className="w-full h-screen bg-[#0a192f]">
+      {/* Container */}
+      <div
+        data-aos="fade-up"
+        className="flex flex-col max-w-[1000px] h-full mx-auto px-8 justify-center "
+      >
+        <p className="text-pink-600 sm:mb-2">Hi, my name is</p>
+        <h1 className="text-5xl sm:text-7xl font-bold text-[#dde5fa] mb-4">
+          Haripriya Chittari{" "}
+          <span className="text-4xl md:text-5xl animate-wave absolute">👋</span>{" "}
+        </h1>
 
+        <h2 className="text-3xl sm:text-5xl font-bold text-pink-600 mb-2">
+          <Typewriter
+            options={{
+              strings: ["Software Developer", "Frontend Developer"],
+              autoStart: true,
+              delay: 180,
+              loop: true,
+            }}
+          />
+        </h2>
 
-
-            <p className='text-[#8892b8] py-4 max-w-[700px] mb-4'>I'm a full stack developer specialized in building (ocassionally designing) exceptional digital experiences. Currently focused on building beautiful and fully responsive websites.</p>
-            <div>
-                <button className='text-white group flex items-center  px-2  md:text-xl py-2  md:py-3 border-2 bg-pink-600 border-pink-600 hover:bg-transparent hover:text-pink-600 rounded-xl'><Link to="work" smooth={true} duration={600} className='px-2 flex'>
-                View Work<span className='group-hover:rotate-90 duration-300'><HiArrowNarrowRight size={25} className='ml-2' />
-                    </span>
-        </Link> </button>
-            </div>
-            <div className=' md:hidden absolute bottom-12 right-0  flex flex-col gap-4  px-4'>
-            <a href="https://www.linkedin.com/in/haripriya-chittari-aa1022159/"  target='_blank' rel='noreferrer' className=' text-blue-700'><FaLinkedin size={30}/></a>
-           <a href="https://github.com/Haripriyachittari" target='_blank' rel='noreferrer' className='text-gray-300'><FaGithub size={30}/></a>
-           <a href={resume}  download className='animate-bounce text-orange-600' ><BsFillPersonLinesFill size={30}/> </a>
-  
-              
-            </div>
-
-
+        <p className="text-[#8892b8] py-4 max-w-[700px] mb-4">
+          I'm a full stack developer specialized in building (ocassionally
+          designing) exceptional digital experiences. Currently focused on
+          building beautiful and fully responsive websites.
+        </p>
+        <div>
+          <button className="text-white group flex items-center  px-2  md:text-xl py-2  md:py-3 border-2 bg-pink-600 border-pink-600 hover:bg-transparent hover:text-pink-600 rounded-xl">
+            <Link to="work" smooth={true} duration={600} className="px-2 flex">
+              View Work
+              <span className="group-hover:rotate-90 duration-300">
+                <HiArrowNarrowRight size={25} className="ml-2" />
+              </span>
+            </Link>{" "}
+          </button>
         </div>
-      
+        <div className=" md:hidden absolute bottom-12 right-0  flex flex-col gap-4  px-4">
+          <a
+            href="https://www.linkedin.com/in/haripriya-chittari-aa1022159/"
+            target="_blank"
+            rel="noreferrer"
+            className=" text-blue-700"
+          >
+            <FaLinkedin size={30} />
+          </a>
+          <a
+            href="https://github.com/Haripriyachittari"
+            target="_blank"
+            rel="noreferrer"
+            className="text-gray-300"
+          >
+            <FaGithub size={30} />
+          </a>
+          <a href={resume} download className="animate-bounce text-orange-600">
+            <BsFillPersonLinesFill size={30} />{" "}
+          </a>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,74 +1,106 @@
-import React, { useEffect } from 'react'
-import {FaUserGraduate,FaGraduationCap } from 'react-icons/fa';
-import {HiOutlineDesktopComputer} from 'react-icons/hi'
+import React, { useEffect } from "react";
+import { FaUserGraduate, FaGraduationCap } from "react-icons/fa";
+import { HiOutlineDesktopComputer } from "react-icons/hi";
 
-import { VerticalTimeline,VerticalTimelineElement } from 'react-vertical-timeline-component';
-import 'react-vertical-timeline-component/style.min.css';
-import Aos from 'aos';
-import 'aos/dist/aos.css'
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 const Timeline = () => {
-  useEffect(()=>{
-    Aos.init({duration:2000})
-   },[])
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
   return (
     <div className=" w-full md:h-screen bg-[#0a192f] text-gray-300 ">
-        <div className="max-w-[1000px]  mx-auto p-4 w-full flex flex-col justify-center h-full">
-          <div className="pb-4 " data-aos='slide-right'>
+      <div className="max-w-[1000px]  mx-auto p-4 w-full flex flex-col justify-center h-full">
+        <div className="pb-4 " data-aos="slide-right">
           <p className="text-4xl font-bold inline border-b-4 border-pink-600">
             Education & Experience
           </p>
-          </div>
-          <div className='w-full'>
-            <VerticalTimeline lineColor='rgb(219 39 119)'>
-           
-  <VerticalTimelineElement
-    className="vertical-timeline-element--work"
-    contentStyle={{ background: '#0a192f', color: 'rgb(209 213 200)',boxShadow:'0 0 3px 3px rgb(29 78 216)' }}
-    contentArrowStyle={{ borderRight: '10px solid rgb(29 78 216)' }}
-    date="2020 - present"
-    iconStyle={{ background: 'rgb(219 39 119)', color: 'rgb(209 213 219)' }}
-    icon={<HiOutlineDesktopComputer/>}
-  >
-    <h3 className="vertical-timeline-element-title font-bold">Software Engineer</h3>
-    <h4 className="vertical-timeline-element-subtitle">Accenture Solutions Pvt Ltd.</h4>
-    <p>
-     Software development,Project Management, Team work,User support.
-    </p>
-  </VerticalTimelineElement>
-  <VerticalTimelineElement
-    className="vertical-timeline-element--work"
-    contentStyle={{ background: '#0a192f', color: 'rgb(209 213 219)',boxShadow:'0 0 3px 3px rgb(29 78 216)' }}
-    contentArrowStyle={{ borderRight: '10px solid  rgb(29 78 216)' }}
-    date="2016 - 2020"
-    iconStyle={{ background: 'rgb(219 39 119)', color: 'rgb(209 213 219)', }}
-    icon={<FaUserGraduate/>}>
-    <h3 className="vertical-timeline-element-title font-bold">National Institute of Engineering (NIE)</h3>
-    <h4 className="vertical-timeline-element-subtitle">Mysore</h4>
-    <p>
-       Completed Bachelor of Engineering in Electronics and Communication.
-    </p>
-         </VerticalTimelineElement>
-         <VerticalTimelineElement
-    className="vertical-timeline-element--work"
-    contentStyle={{ background: '#0a192f', color: 'rgb(209 213 219)' ,boxShadow:'0 0 3px 3px rgb(29 78 216)'}}
-    contentArrowStyle={{ borderRight: '10px solid  rgb(29 78 216)' }}
-    date="2014 - 2016"
-    iconStyle={{ background: 'rgb(219 39 119)', color: 'rgb(209 213 219)' }}
-    icon={<FaGraduationCap/>}>
-    <h3 className="vertical-timeline-element-title font-bold">Alva's PU college</h3>
-    <h4 className="vertical-timeline-element-subtitle">Mangaluru</h4>
-    <p>
-      Completed Pre-university education in PCMB.
-    </p>
-         </VerticalTimelineElement>
-             </VerticalTimeline>
-
         </div>
-       </div>
-      
+        <div className="w-full">
+          <VerticalTimeline lineColor="rgb(219 39 119)">
+            <VerticalTimelineElement
+              className="vertical-timeline-element--work"
+              contentStyle={{
+                background: "#0a192f",
+                color: "rgb(209 213 200)",
+                boxShadow: "0 0 3px 3px rgb(29 78 216)",
+              }}
+              contentArrowStyle={{ borderRight: "10px solid rgb(29 78 216)" }}
+              date=" Nov 2020 - Jul 2022 "
+              iconStyle={{
+                background: "rgb(219 39 119)",
+                color: "rgb(209 213 219)",
+              }}
+              icon={<HiOutlineDesktopComputer />}
+            >
+              <h3 className="vertical-timeline-element-title font-bold">
+                Software Engineer
+              </h3>
+              <h4 className="vertical-timeline-element-subtitle">
+                Accenture Solutions Pvt Ltd.
+              </h4>
+              <p>
+                Frontend development,Managing UI,Reusable components,Building
+                Responsive designs, worked with React JS | Javascript |
+                HTML/CSS3.
+              </p>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+              className="vertical-timeline-element--work"
+              contentStyle={{
+                background: "#0a192f",
+                color: "rgb(209 213 219)",
+                boxShadow: "0 0 3px 3px rgb(29 78 216)",
+              }}
+              contentArrowStyle={{ borderRight: "10px solid  rgb(29 78 216)" }}
+              date="2016 - 2020"
+              iconStyle={{
+                background: "rgb(219 39 119)",
+                color: "rgb(209 213 219)",
+              }}
+              icon={<FaUserGraduate />}
+            >
+              <h3 className="vertical-timeline-element-title font-bold">
+                National Institute of Engineering (NIE)
+              </h3>
+              <h4 className="vertical-timeline-element-subtitle">Mysore</h4>
+              <p>
+                Completed Bachelor of Engineering in Electronics and
+                Communication.
+              </p>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+              className="vertical-timeline-element--work"
+              contentStyle={{
+                background: "#0a192f",
+                color: "rgb(209 213 219)",
+                boxShadow: "0 0 3px 3px rgb(29 78 216)",
+              }}
+              contentArrowStyle={{ borderRight: "10px solid  rgb(29 78 216)" }}
+              date="2014 - 2016"
+              iconStyle={{
+                background: "rgb(219 39 119)",
+                color: "rgb(209 213 219)",
+              }}
+              icon={<FaGraduationCap />}
+            >
+              <h3 className="vertical-timeline-element-title font-bold">
+                Alva's PU college
+              </h3>
+              <h4 className="vertical-timeline-element-subtitle">Mangaluru</h4>
+              <p>Completed Pre-university education in PCMB.</p>
+            </VerticalTimelineElement>
+          </VerticalTimeline>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Timeline
+export default Timeline;
